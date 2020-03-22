fcea4837b7b60c1184e561eaf9f87ab72f2dc468
Logan Rouleau <loganrouleau@users.noreply.github.com>
2020-03-22 10:41:12 -0700

    Added test db initialization script

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-const Influx = require('influx');
-// const influx = new Influx.InfluxDB('http://localhost:8086')
-// influx.createDatabase('express_response_db')
-const influx = new Influx.InfluxDB({
- host: 'localhost',
- port: '8086',
- database: 'express_response_db',
- schema: [
-   {
-     measurement: 'response_times',
-     fields: {
-       path: Influx.FieldType.STRING,
-       duration: Influx.FieldType.INTEGER
-     },
-     tags: [
-       'host'
-     ]
-   }
- ]
-})
-influx.createDatabase('express_response_db')
+const Influx = require("influx");
+const influxDbAddress = "http://localhost:8086";
+const testDb = "test";
+const testMeasurement = "bot_reading";
+const influx = new Influx.InfluxDB(influxDbAddress);
+
+initializeTestDb();
+
+async function initializeTestDb() {
+  await dropTestDb();
+  await influx.createDatabase(testDb);
+  influx.addSchema({
+    database: testDb,
+    measurement: testMeasurement,
+    fields: {
+      temp: Influx.FieldType.FLOAT,
+      humidity: Influx.FieldType.FLOAT
+    },
+    tags: []
+  });
+  await influx.writePoints(
+    [
+      {
+        measurement: testMeasurement,
+        fields: { temp: 36.5 },
+        timestamp: new Date(Date.now() - 1000)
+      },
+      {
+        measurement: testMeasurement,
+        fields: { temp: 37.7 },
+        timestamp: new Date()
+      }
+    ],
+    { database: testDb }
+  );
+}
+
+async function dropTestDb() {
+  let names = await influx.getDatabaseNames();
+  if (names.includes(testDb)) {
+    await influx.dropDatabase(testDb);
+  }
+}
