a3ac7d624f7db0095588f449102229932814c586
Logan Rouleau <loganrouleau@users.noreply.github.com>
2020-03-25 19:55:19 -0700

    Write data from mock-server to influx every 5 seconds

--- agent/data-collector.js
+++ agent/data-collector.js
@@ -1,10 +1,19 @@
-var net = require("net");
+const Influx = require("influx");
+const net = require("net");
+const influxDbAddress = "http://localhost:8086";
+const testDb = "test";
+const testMeasurement = "bot_reading";
+const influx = new Influx.InfluxDB(influxDbAddress);
 
-run();
+initializeTestDb();
+
+setInterval(function() {
+  run();
+}, 5000);
 
 async function run() {
   let data = await getReading();
-  writeToDb(data);
+  await writeToDb(data);
 }
 
 function getReading() {
@@ -19,10 +28,51 @@ function getReading() {
   });
 }
 
-function writeToDb(data) {
+async function writeToDb(data) {
   let fields = JSON.parse(data);
   let timestamp = Date.parse(fields.created_at) * 1000000;
   delete fields.created_at;
   console.log(fields);
   console.log(timestamp);
+  await influx.writePoints(
+    [
+      {
+        measurement: testMeasurement,
+        fields: fields,
+        //timestamp: timestamp
+        timestamp: Date.now() * 1000000
+      }
+    ],
+    { database: testDb }
+  );
+}
+
+async function initializeTestDb() {
+  await dropTestDb();
+  await influx.createDatabase(testDb);
+  influx.addSchema({
+    database: testDb,
+    measurement: testMeasurement,
+    fields: {
+      temp_val: Influx.FieldType.FLOAT,
+      humi_val: Influx.FieldType.INTEGER,
+      light_val: Influx.FieldType.FLOAT,
+      power_vol_val: Influx.FieldType.FLOAT,
+      ssid: Influx.FieldType.STRING,
+      rssi: Influx.FieldType.FLOAT,
+      acce_xval: Influx.FieldType.FLOAT,
+      acce_yval: Influx.FieldType.FLOAT,
+      acce_zval: Influx.FieldType.FLOAT,
+      mag_val: Influx.FieldType.FLOAT,
+      ext_temp_val: Influx.FieldType.FLOAT
+    },
+    tags: []
+  });
+}
+
+async function dropTestDb() {
+  let names = await influx.getDatabaseNames();
+  if (names.includes(testDb)) {
+    await influx.dropDatabase(testDb);
+  }
 }
